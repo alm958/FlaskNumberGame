d713298aabe4bd0fe89bfbad249ed4bad7ca3459
alm958 <alm958@gmail.com>
2016-09-13 20:10:13 -0400

    commit working version

--- numgameserver.py
+++ numgameserver.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, session
+from flask import Flask, request, render_template, redirect, session, url_for
 import random
 
 app = Flask(__name__)
@@ -6,22 +6,37 @@ app.secret_key = 'mykey'
 
 @app.route('/', methods=['get','post'])
 def count():
+    message = None
     error = None
+    session['guess_count'] = 0
     session['number']= random.randint(1,100)
-    session['number']= 50
-    return render_template('index.html')
+    return render_template('index.html', message = message)
 
 @app.route('/guess', methods=['post'])
 def guess():
+    try:
+        val = int(request.form['guess'])
+    except ValueError:
+        error = "That's not an integer! Enter an integer between 1 and 100."
+        return render_template('index.html', error = error)
+    session['guess_count'] += 1
     if int(request.form['guess']) > session['number']:
-        error = 'Too high. Enter a smaller number.'
-        return redirect('/')
+        message = 'Too high. Enter a smaller number. Your guess count so far is '
+        return render_template('index.html', message = message)
     elif int(request.form['guess']) < session['number']:
-        error = 'Too low. Enter a larger number.'
-        return redirect('/')
+        message = 'Too low. Enter a larger number. Your guess count so far is '
+        return render_template('index.html', message = message)
     elif int(request.form['guess']) == session['number']:
-        error = 'Correct! Congratulations!'
-        return render_template('index.html')
+        message = 'Correct! Congratulations! Your total guesses was '
+        return render_template('index.html', message = message)
+
+@app.route('/reset', methods=['post'])
+def reset():
+    message = None
+    error = None
+    session['guess_count'] = 0
+    session['number'] = random.randint(1,100)
+    return render_template('index.html', message = message)
 
 if __name__ == '__main__':
     app.run(debug=True)
